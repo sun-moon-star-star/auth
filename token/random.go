664e3ee626ff151451ef9e383a4779b808ad07f4
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-02-04 21:10:15 +0800

    feat(token/random): reduce the probability of uuid conflicts caused by multiple instances restarting at the same time in distributed scenarios

--- token/random.go
+++ token/random.go
@@ -4,13 +4,13 @@ import (
     "fmt"
     "math/rand"
     "time"
+    "os"
 )
 
 func RandSeedTime() {
-    rand.Seed(time.Now().Unix())
+    rand.Seed(time.Now().Unix()^ int64(os.Getpid()))
 }
 
-// 注意: 如果分布式服务同时重启有一定的概率导致id冲突, 需要注意RandSeedTime的调用时机
 // 格式: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
 func GetUUID() (string, error) {
     b := make([]byte, 16)
