dca728b3dfb1fc7963806e68660b4110cf9ad497
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-02-21 14:20:37 +0800

    test(token_pool/token_pool_test): TestTokenPoolCheckStrategy

--- token_pool/token_pool.go
+++ token_pool/token_pool.go
@@ -32,8 +32,9 @@ type TokenPool struct {
 type TokenPoolOption struct {
 	DefaultExpireSeconds uint32
 	DefaultKey           []byte
-	PushStrategy         Strategy
-	CheckStrategy        Strategy
+
+	PushStrategy  Strategy
+	CheckStrategy Strategy
 }
 
 var defaultTokenPoolOption = TokenPoolOption{
--- token_pool/token_pool_test.go
+++ token_pool/token_pool_test.go
@@ -1,6 +1,8 @@
 package token_pool
 
 import (
+	"auth/token"
+	"strings"
 	"testing"
 	"time"
 )
@@ -10,7 +12,6 @@ func TestTokenPoolBase(t *testing.T) {
 	pool.DefaultExpireSeconds = 2
 
 	token := pool.GenerateTokenID()
-	t.Log(token)
 	if err := pool.Check(token); err != nil {
 		t.Fatal(err)
 	}
@@ -26,7 +27,6 @@ func TestTokenPoolExpired(t *testing.T) {
 	pool.DefaultExpireSeconds = 2
 
 	token := pool.GenerateTokenID()
-	t.Log(token)
 	if err := pool.Check(token); err != nil {
 		t.Fatal(err)
 	}
@@ -34,5 +34,41 @@ func TestTokenPoolExpired(t *testing.T) {
 	pool.Expired(TokenFlag{ID: token.ID, ExpireTime: token.ExpireTime})
 	if err := pool.Check(token); err == nil {
 		t.Fatal("check strategy failed")
+	}
+}
+
+func TestTokenPoolCheckStrategy(t *testing.T) {
+	pool := New()
+	pool.DefaultExpireSeconds = 2
+	pool.PushStrategy = func(token *token.Token, pool *TokenPool) bool {
+		return false
+	}
+	pool.CheckStrategy = func(token *token.Token, pool *TokenPool) bool {
+		value, ok := token.Info["version"]
+		if !ok {
+			return false
+		}
+
+		if strings.Compare(value, "1.0") == 0 {
+			return false
+		}
+
+		return token.Check(pool.DefaultKey)
+	}
+
+	info := map[string]string{
+		"version": "1.0",
+		"name":    "sun-moon-star-star",
+	}
+
+	token := pool.GenerateTokenNoCopyInfo(info)
+	if err := pool.Check(token); err == nil {
+		t.Fatal(err)
+	}
+
+	info["version"] = "2.0"
+	t.Log(token)
+	if err := pool.Check(token); err == nil {
+		t.Fatal(err)
 	}
 }
